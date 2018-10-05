eca19a335c2bd60e1f927767187141385b72c416
cxlhyphen <42073246+cxlhyphen@users.noreply.github.com>
2018-10-05 02:21:38 -0400

    finished files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,67 @@
-
+//Global variables
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var userGuess = "";
+var computerGuess = "";
+
+var wrongLetters = [];
 
 var wins = 0;
 var losses = 0;
+var chancesLeft = 9;
 
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var chancesText = document.getElementById("chances");
 var guessedText = document.getElementById("guessed");
 
+document.getElementById("chances").innerHTML = chancesLeft;
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
 
+//Functions
 
-document.onkeyup = function (event) {
-    var userGuess = event.key;
+function gamestart() {
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    emptyGuesses();
+};
 
-    console.log("user guessed: " + userGuess);
+function emptyGuesses() {
+    wrongLetters = [];
+    guessedText.innerHTML = wrongLetters;
+};
 
-    console.log("computer guessed: " + computerGuess);
-
-
+function compareGuess() {
     if (userGuess === computerGuess) {
         wins++;
-        winsText.textContent = wins;
-    }
-    
-    else {
-        guessedText.textContent = userGuess + ", ";
+        winsText.innerHTML = wins;
+        alert("You Won! Onto The Next Round!")
+        gamestart();
     }
 
-}
+    else {
+        wrongLetters.push(" " + userGuess);
+        chancesLeft--;
+        chancesText.innerText = chancesLeft;
+        guessedText.innerHTML = wrongLetters;
+    }
+
+    if (chancesLeft == 0) {
+        alert("You Lost! Onto The Next Round!")
+        chancesLeft = 9;
+        chancesText.innerText = chancesLeft;
+        losses++;
+        lossesText.innerHTML = losses;
+        gamestart();
+    };
+};
+
+//Main Process
+
+gamestart();
+
+document.onkeyup = function (event) {
+    userGuess = event.key;
+    compareGuess();
+};
